58e89bbce1f2225c56a01a81ca5b2b8b62d52496
kamalrana0303 <kamalrana03@gmail.com>
2021-03-07 01:15:19 -0800

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, VERSION } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: [ './app.component.css' ],
+  animations:[
+    trigger('animation',[
+      state('true',style({
+        height:'200px',
+        opacity:1
+      })),
+      state('false',style({
+        height:'100px',
+        opacity:0.5
+      })),
+      transition('true=>false',[
+        animate('1s')
+      ]),
+      transition("false=>true",[
+        animate("0.5s")
+      ])
+      ]
+    )
+  ]
 })
 export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+  start:boolean=false;
 }
